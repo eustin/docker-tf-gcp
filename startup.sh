a063b073c3301281939865cc32b5b4302622528c
eustin <eustin.jvans@gmail.com>
2020-04-07 19:33:52 +1000

    remove comment referring to incorrect user

--- startup.sh
+++ startup.sh
@@ -19,7 +19,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# no more 'sudo docker' after this. $USER evaluates to 'docker' during startup.
+# no more 'sudo docker' after this
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
