ff2fd7ce6942c065d935e9945b95a1e213e3a99e
eustin <eustin.jvans@gmail.com>
2020-04-07 19:14:11 +1000

    apt update and install in one line

--- startup.sh
+++ startup.sh
@@ -5,12 +5,10 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get install -y cuda-drivers
+sudo apt-get update && sudo apt-get install -y cuda-drivers
 
 # install docker
-sudo apt-get update
-sudo apt-get install -y \
+sudo apt-get update && apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
