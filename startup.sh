47468dfc5f874a6908e7d219641b3948e3090bd0
eustin <eustin.jvans@gmail.com>
2020-04-07 19:14:40 +1000

    apt update and install on same line in startup script

--- startup.sh
+++ startup.sh
@@ -17,8 +17,7 @@ sudo apt-get update && apt-get install -y \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # no more 'sudo docker' after this. $USER evaluates to 'docker' during startup.
 sudo groupadd docker
